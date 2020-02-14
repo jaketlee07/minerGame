338dabedd18effd2e102b49d8bfdc0e2dcee9cd3
Jake Lee <jaketl2002@gmail.com>
2020-02-14 11:40:27 -0600

    Screen and aspect ratio

---
+++ core/src/com/jake/main/main/Screens/PlayScreen.java
@@ -0,0 +1,70 @@
+package com.jake.main.main.Screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.jake.main.main.MyGame;
+import com.sun.org.apache.xpath.internal.operations.Or;
+
+public class PlayScreen  implements Screen {
+    private MyGame game;
+    Texture texture;
+    private OrthographicCamera gamecam;
+    private Viewport gamePort;
+
+    public PlayScreen(MyGame game)
+    {
+        this.game = game;
+        texture = new Texture("badlogic.jpg");
+        gamecam = new OrthographicCamera();
+        gamePort = new FitViewport(800,480,gamecam);
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta)
+        {
+            Gdx.gl.glClearColor(1,0,0,1);
+            Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+            game.batch.setProjectionMatrix(gamecam.combined);
+            game.batch.begin();
+            game.batch.draw(texture,0,0);
+            game.batch.end();
+        }
+
+    @Override
+    public void resize(int width, int height)
+    {
+        gamePort.update(width,height);
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
