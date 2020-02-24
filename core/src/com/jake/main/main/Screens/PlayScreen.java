fd0156248e748115be7b32e79570eb31d62501e8
Jake Lee <jaketl2002@gmail.com>
2020-02-24 11:58:30 -0600

    test map

--- core/src/com/jake/main/main/Screens/PlayScreen.java
+++ core/src/com/jake/main/main/Screens/PlayScreen.java
@@ -1,5 +1,6 @@
 package com.jake.main.main.Screens;
 
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
@@ -8,6 +9,13 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
@@ -20,9 +28,14 @@ public class PlayScreen  implements Screen {
     private OrthographicCamera gamecam;
     private Viewport gamePort;
 
+    //Tiled map variables
     private TmxMapLoader mapLoader;
     private TiledMap map;
     private OrthogonalTiledMapRenderer renderer;
+
+    //Box2d variable
+    private World world;
+    private Box2DDebugRenderer b2dr;
 
     public PlayScreen(MyGame game)
     {
@@ -30,12 +43,21 @@ public class PlayScreen  implements Screen {
         // create cam to follow character through map
         gamecam = new OrthographicCamera();
         // create a FitViewport to maintain virtual aspect ration despite device screen size
-        gamePort = new FitViewport(800,480,gamecam);
+        gamePort = new FitViewport(800, 480,gamecam);
 
         mapLoader = new TmxMapLoader();
-        map = mapLoader.load("testmap.tmx");
+        map = mapLoader.load("level1.tmx");
         renderer = new OrthogonalTiledMapRenderer(map);
         gamecam.position.set(gamePort.getWorldWidth() / 2, gamePort.getWorldHeight()/2 , 0);
+
+        world = new World(new Vector2(0,0), true);
+        b2dr = new Box2DDebugRenderer();
+
+        BodyDef bdef = new BodyDef();
+        PolygonShape shape = new PolygonShape();
+        FixtureDef fdef = new FixtureDef();
+        Body body;
+
     }
 
     @Override
